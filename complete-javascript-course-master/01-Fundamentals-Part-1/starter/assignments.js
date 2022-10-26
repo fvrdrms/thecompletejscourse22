d5c9527143ed395884027776e0eb6c6f08d9c2b6
nick <nick@feverdreams.io>
2022-10-26 10:24:54 -0500

    complete 2.17 strings and template literals + assignment

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -1,27 +1,35 @@
+// let country = 'United States';
+// let continent = 'North America';
+// let population = 331;
+
+// console.log(country)
+// console.log(continent)
+// console.log(population)
+
+// const isIsland = false;
+// let language;
+
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
+
+// language = 'English';
+
+// // isIsland = true;
+
+// console.log(population / 2);
+// population++;
+// console.log(population);
+// const finland = 6;
+// console.log(country > finland);
+// const description = country + ' ' + 'is' + ' ' + 'in' + ' ' + continent + ',' + ' and its' + ' ' + population + ' ' + 'million people speak' + ' ' + language;
+// console.log(description);
+
 let country = 'United States';
 let continent = 'North America';
 let population = 331;
+let language = 'English';
 
-console.log(country)
-console.log(continent)
-console.log(population)
-
-const isIsland = false;
-let language;
-
-console.log(typeof isIsland);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof language);
-
-language = 'English';
-
-// isIsland = true;
-
-console.log(population / 2);
-population++;
-console.log(population);
-const finland = 6;
-console.log(country > finland);
-const description = country + ' ' + 'is' + ' ' + 'in' + ' ' + continent + ',' + ' and its' + ' ' + population + ' ' + 'million people speak' + ' ' + language;
+const description = `${country} is in ${continent} and its ${population} million people speak the ${language} language.`;
 console.log(description);
--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,19 +1,39 @@
-// let js = 'amazing';
-// console.log(40 + 8 + 23 - 10);
+// // let js = 'amazing';
+// // console.log(40 + 8 + 23 - 10);
 
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2018;
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
 
-console.log(now - 1991 > now - 2018);
+// console.log(now - 1991 > now - 2018);
 
-console.log(25 - 10 - 5);
+// console.log(25 - 10 - 5);
 
-let x, y;
-x = y = 25 - 10 - 5; // x = y = 10, x = 10
-console.log(x, y);
+// let x, y;
+// x = y = 25 - 10 - 5; // x = y = 10, x = 10
+// console.log(x, y);
 
 
-const averageAge = (ageJonas + ageSarah) / 2;
-console.log(ageJonas, ageSarah);
-console.log(averageAge);
+// const averageAge = (ageJonas + ageSarah) / 2;
+// console.log(ageJonas, ageSarah);
+// console.log(averageAge);
+
+const firstName = 'Nick';
+const job = 'sales rep';
+const birthYear = 1991;
+const year = 2022;
+const nick = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
+console.log(nick);
+
+// template literals
+const nickNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+console.log(nickNew);
+
+console.log(`This is just a regular string.`);
+
+console.log(`This
+is
+a
+multiple
+line
+string.`);
