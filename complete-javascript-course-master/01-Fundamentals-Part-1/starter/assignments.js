e9a8baed3d1499dd5f10895ff468dc0ded780ee3
nick <nick@feverdreams.io>
2022-10-25 21:54:05 -0500

    initial commit

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,21 @@
+let country = 'United States';
+let contintent = 'North America';
+let population = 331;
+
+console.log(country)
+console.log(contintent)
+console.log(population)
+
+const isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+language = 'English';
+
+// isIsland = true;
+
+console.log(population / 2);
