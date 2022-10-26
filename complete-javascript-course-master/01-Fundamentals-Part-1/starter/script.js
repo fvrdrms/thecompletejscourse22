89ccaa79524849f767a51e64092bc08c12f7122e
nick <nick@feverdreams.io>
2022-10-26 12:35:34 -0500

    complete 2.21 truthy and falsy values (no assignment)

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -58,20 +58,43 @@
 // console.log(century);
 
 
-// type conversion
-const inputYear = '1991';
-console.log(Number(inputYear));
-console.log(Number(inputYear) + 18);
+// // type conversion
+// const inputYear = '1991';
+// console.log(Number(inputYear));
+// console.log(Number(inputYear) + 18);
 
-// type coercion
-console.log('I am ' + 23 + ' years old.')
-// is the same as writing:
-console.log('I am ' + '23' + ' years old.')
-// strings to numbers because of the -
-console.log('23' - '3' - 10);
-// the + causes concatonation
-console.log('23' + '3' + 10);
-// the * multiples because js knows it is a math operation
-console.log('28' * '2');
-// same is true for / division
-console.log('28' / '2');
+// // type coercion
+// console.log('I am ' + 23 + ' years old.')
+// // is the same as writing:
+// console.log('I am ' + '23' + ' years old.')
+// // strings to numbers because of the -
+// console.log('23' - '3' - 10);
+// // the + causes concatonation
+// console.log('23' + '3' + 10);
+// // the * multiples because js knows it is a math operation
+// console.log('28' * '2');
+// // same is true for / division
+// console.log('28' / '2');
+
+// 5 falsy values = 0, '', undefined, null, NaN
+// any other value is truthy and will be converted to true when converted to a boolean
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 10;
+
+if (money) {
+    console.log(`Don't spend it all.`);
+} else {
+    console.log(`You should get a job.`);
+}
+let height = 1;
+if (height) {
+    console.log(`Yay! Height is defined!`);
+} else {
+    console.log(`Height is undefined.`);
+}
