b29674b664028190d1a04d95fd8edb70cf884b40
nick <nick@feverdreams.io>
2022-10-25 23:05:36 -0500

    began 2.15 operator precedence

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,2 +1,14 @@
-let js = 'amazing';
-console.log(40 + 8 + 23 - 10);
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarag = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+console.log(25 - 10 - 5);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
