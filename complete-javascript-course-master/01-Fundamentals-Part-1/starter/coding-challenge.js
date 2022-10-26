a7c3aa98a32e7c53c86948d12491d393e1d56022
nick <nick@feverdreams.io>
2022-10-26 11:25:29 -0500

    complete coding challenge 2 in coding-challenge.js

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/coding-challenge.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/coding-challenge.js
@@ -1,31 +1,37 @@
-let massMark = 78;
-let heightMark = 1.69;
-let massJohn = 92;
-let heightJohn = 1.95;
+// redundant code
+//
+// let massMark = 78;
+// let heightMark = 1.69;
+// let massJohn = 92;
+// let heightJohn = 1.95;
 
-console.log(massMark, heightMark, massJohn, heightJohn);
+// console.log(massMark, heightMark, massJohn, heightJohn);
 
-let bmiMark = massMark / heightMark ** 2;
-console.log(bmiMark);
+// let bmiMark = massMark / heightMark ** 2;
+// console.log(bmiMark);
 
-let bmiJohn = massJohn / heightJohn ** 2;
-console.log(bmiJohn);
+// let bmiJohn = massJohn / heightJohn ** 2;
+// console.log(bmiJohn);
 
-let markHigherBMI = bmiMark > bmiJohn;
-console.log(markHigherBMI);
+// let markHigherBMI = bmiMark > bmiJohn;
+// console.log(markHigherBMI);
 
-massMark = 95;
+massMark = 12;
 heightMark = 1.88;
 massJohn = 85;
 heightJohn = 1.76;
 
-console.log(massMark, heightMark, massJohn, heightJohn);
-
-bmiMark = massMark / heightMark ** 2;
+bmiMark = Math.round(massMark / heightMark ** 2);
 console.log(bmiMark);
 
-bmiJohn = massJohn / heightJohn ** 2;
+bmiJohn = Math.round(massJohn / heightJohn ** 2);
 console.log(bmiJohn);
 
-markHigherBMI = bmiMark > bmiJohn;
-console.log(markHigherBMI);
+// markHigherBMI = bmiMark > bmiJohn;
+// console.log(markHigherBMI);
+
+if (bmiJohn > bmiMark) {
+    console.log(`John has a higher BMI(${bmiJohn}) than Mark(${bmiMark})!`);
+} else {
+    console.log(`Mark has a higher BMI(${bmiMark}) than John(${bmiJohn})!`);
+}
