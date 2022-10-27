aaeb7b25b5d3b33cc449d9ba1a1c3664ffdea4c2
nick <nick@feverdreams.io>
2022-10-27 07:24:50 -0500

    3.33 functions

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,10 +1,30 @@
-'use strict';
+// 'use strict';
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriverLicense = true;
-if (hasDriversLicense) console.log('I can drive!');
+// if (passTest) hasDriverLicense = true;
+// if (hasDriversLicense) console.log('I can drive!');
 
-// const interface = 'Audio';
-// const private = 524;
+// // const interface = 'Audio';
+// // const private = 524;
+
+function logger() {
+    console.log('My name is Jonas.');
+}
+
+// calling, running, or invoking the function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
