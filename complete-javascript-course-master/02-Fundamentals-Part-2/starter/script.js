b8da958a00cdfe528a3bc04a403bc2d12b861f4d
nick <nick@feverdreams.io>
2022-10-28 20:58:25 -0500

    complete lesson 3.39 introduction to arrays

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -102,8 +102,8 @@ const friend3 = 'Peter';
 const friends = ['Michael', 'Steven', 'Peter'];
 console.log(friends);
 
-const years = new Array(1991, 1992, 1984, 2008, 2022); // not the usual way to create an array
-console.log(years);
+// const years = new Array(1991, 1992, 1984, 2008, 2022); // not the usual way to create an array
+// console.log(years);
 
 console.log(friends[0]); // logs first element of array
 console.log(friends[2]); // logs third element of array
@@ -113,3 +113,20 @@ console.log(friends[friends.length - 1]); // gives last element of array
 
 friends[2] = 'Jay';
 console.log(friends);
+
+const firstName = 'Jonas';
+const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+console.log(jonas.length);
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const years = [1990, 1967, 2002, 2010, 2018];
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])]
+console.log(ages);
