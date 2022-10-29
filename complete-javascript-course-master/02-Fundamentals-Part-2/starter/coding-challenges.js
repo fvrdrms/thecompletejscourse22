934e555b467c57fed0427d2ce6e5eb8760d5941c
nick <nick@feverdreams.io>
2022-10-28 20:01:06 -0500

    complete coding challenge 2.1

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/coding-challenges.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/coding-challenges.js
@@ -1,9 +1,19 @@
 'use strict';
 
-const calcAverage = (, dolphinsAvgScore) => {
+const calcAverage = (a, b, c) => (a + b + c) / 3;
 
-    return;
+const scoreDolphins = calcAverage(44, 23, 71);
+const scoreKoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+    if (avgDolphins >= 2 * avgKoalas) {
+        console.log(`Dolphins win the trophy! ${avgDolphins} vs. ${avgKoalas}.`);
+    } else if (avgKoalas >= 2 * avgDolphins) {
+        console.log(`Koalas win the trophy! ${avgKoalas} vs. ${avgDolphins}.`);
+    } else {
+        console.log('No team wins.');
+    }
 }
 
-koalasAvgScore(100, 101, 102);
-dolphinsAvgScore(103, 104, 105);
+checkWinner(576, 111);
