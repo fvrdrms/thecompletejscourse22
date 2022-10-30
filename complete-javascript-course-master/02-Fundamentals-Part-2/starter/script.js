64c9a39848fa6310c9547f26ee18f9686d7ed598
nick <nick@feverdreams.io>
2022-10-29 22:43:34 -0500

    did not stage all changes, fixed

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -179,21 +179,60 @@
 
 // fruitProcessor(2, 3);
 
+// const calcAge = function (birthYear) {
+//     return 2037 - birthYear;
+// };
+
+// const yearsUntilRetirement = function (birthYear, firstName) {
+//     const age = calcAge(birthYear);
+//     const retirement = 65 - age;
+//     if (retirement > 0) {
+//         console.log(`${firstName} retires in ${retirement} years.`);
+//         return retirement;
+//     } else {
+//         console.log(`${firstName} has already retired!`);
+//         return -1;
+//     }
+// };
+
+// console.log(yearsUntilRetirement(1991, 'Nick'));
+// console.log(yearsUntilRetirement(1950, 'Mike'));
+
+const friend1 = 'Michael';
+const friend2 = 'Steven';
+const friend3 = 'Peter';
+
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+// const years = new Array(1991, 2001, 2021, 2045); // more usual to use literal syntax (brackets)
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]); // reminder - an expression produces a value
+
+friends[2] = 'Jay'; // can mutate arrays but not replace an entire array
+console.log(friends);
+
+const firstName = 'Jonas';
+const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+console.log(jonas);
+console.log(jonas, length);
+
+// exercise
+
 const calcAge = function (birthYear) {
     return 2037 - birthYear;
 };
 
-const yearsUntilRetirement = function (birthYear, firstName) {
-    const age = calcAge(birthYear);
-    const retirement = 65 - age;
-    if (retirement > 0) {
-        console.log(`${firstName} retires in ${retirement} years.`);
-        return retirement;
-    } else {
-        console.log(`${firstName} has already retired!`);
-        return -1;
-    }
-};
+const years = [1990, 1967, 2002, 2010, 2018];
 
-console.log(yearsUntilRetirement(1991, 'Nick'));
-console.log(yearsUntilRetirement(1950, 'Mike'));
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
